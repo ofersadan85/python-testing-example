03271683892af66aec4cfacd01282a62c018e5d1
Ofer Sadan <ofersadan85@gmail.com>
2023-11-29 18:39:37 +0200

    added main.py and tests

---
+++ main.py
@@ -0,0 +1,14 @@
+# import unittest
+# pip install pytest
+
+
+def something():
+    return 4
+
+
+def add(a: int, b: int) -> int:
+    return int(a + b)
+
+
+def square(x: int) -> int:
+    return x * x
---
+++ main_test.py
@@ -0,0 +1,40 @@
+import pytest
+from main import *
+
+
+def test_something():
+    result = something()
+    assert result == 4
+    assert type(result) == int
+
+
+@pytest.mark.parametrize(
+        ("input_args", "expected_result"),
+        [
+            ((2, 3), 5),
+            ((1, 3), 4),
+            ((3.0, 4.5), 7)
+        ])
+def test_add(input_args, expected_result):
+    # print(input_args)
+    # print(expected_result)
+    result = add(*input_args)
+    assert result == expected_result
+    assert type(result) == int
+
+
+def test_square():
+    assert square(2) == 4
+    assert square(10) == 100
+
+
+def test_everything():
+    number = something()
+    assert number == 4
+    number = add(number, 3)
+    assert number == 7
+    number = square(number)
+    assert number == 49
+
+def test_no_error():
+    assert 1 == 1
